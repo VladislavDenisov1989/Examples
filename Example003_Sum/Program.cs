ca1d0fb6d847f13d1c7cfbb5be595a3cfd7a1d2e
VladislavDenisov1989 <123163827+VladislavDenisov1989@users.noreply.github.com>
2023-02-10 17:54:56 +0300

    Example003

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,4 @@
+﻿int numberA = 31;                  // int - целое число, number - цифра
+int numberB = 5;                   // int - целое число, number - цифра
+int result = numberA + numberB;    // результат = цифраА + цифраБ
+Console.WriteLine(result);         // консоль пиши в линию (результат)
