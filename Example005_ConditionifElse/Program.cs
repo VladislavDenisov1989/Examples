9e902a7fceb9ceb1b7774156e332f5f89017d57c
VladislavDenisov1989 <123163827+VladislavDenisov1989@users.noreply.github.com>
2023-02-11 18:24:14 +0300

    Example005

---
+++ Example005_ConditionifElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.RedLine();
+
+if(username == "Влад");
+{
+    Console.Writeline("Ура, это же Влад");
+}
+else
+{
+    ConsoleWrite("Привет, ");
+    ConsoleWriteLine(username);
+}
