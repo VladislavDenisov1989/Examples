b0970f3dffcab9ab180c54c93745ca720bd4b819
VladislavDenisov1989 <123163827+VladislavDenisov1989@users.noreply.github.com>
2023-02-24 00:40:43 +0300

    Exother

---
+++ Exother/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Linq;
+
+var a = 12;       // Можно прописать int a = 12 но не стоит если проект большой лучше использовать var;
+//Console.WriteLine(a.GetType().Name);
+
+var data = new int[]{1,2,3,4}
+         .Where(e=> e>0)
+         .Select(e => new { q = e, w = e + 1 });
+Console.WriteLine(data.GetType());        // Console.WriteLine(data.GetType().Name); 
